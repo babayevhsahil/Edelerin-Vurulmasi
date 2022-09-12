1e49ff763c2e18c53ff320430e9245a1435e3424
sahilbabayev44 <107620978+sahilbabayev44@users.noreply.github.com>
2022-09-13 00:05:48 +0400

    Edelerin Hasili

---
+++ Js/app.js
@@ -0,0 +1,6 @@
+function hesabla()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
